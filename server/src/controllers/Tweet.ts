02cb24947a30f7893a24fe62f338df09ab181665
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-04-07 09:20:47 +0530

    refactor: update Tweet and Comment interfaces for improved structure and clarity

--- server/src/controllers/Tweet.ts
+++ server/src/controllers/Tweet.ts
@@ -1,18 +1,25 @@
+import CommentModel from "../model/CommentSchema";
 import TweetModel from "../model/TweetSchema";
 
-interface TweetProp{
+interface TweetProp {
+    content: string;
+    createdAt: Date;
+    user: object // Assuming user is a string (e.g., user ID)
+    like: Array<string>; // Assuming like is an array of strings (e.g., user IDs)
+}
+
+interface CommentTweetProp{
     content: string,
-    createdAt: Date
-    user: string
-    like: Array<any>
+    createdAt: Date,
+    user: string,
+    
 }
 export const PostTweet = async(req:{body:TweetProp}, res:any) => {
-    const { content,user,createdAt } = req.body;
+    const { content,user } = req.body;
     try {
         const tweets = await TweetModel.create({
-            content,
-            user,
-            createdAt
+            content:content,
+            user:user,
         })
         return res.status(200).json("tweet successfully", tweets);
     } catch (e) {
@@ -21,6 +28,46 @@ export const PostTweet = async(req:{body:TweetProp}, res:any) => {
     }
 }
 
-export const AllTweets = async (req, res) => {
-    
+export const AllTweets = async (req:any,res:any) => {
+    try {
+        const tweets = await TweetModel.find().populate('user', 'username');
+        return res.status(200).json("fetch all tweets",tweets)
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json("Internal server error while fetching all tweets")
+    }
+
 }
+
+export const commentTweet = async (req: any, res: any) => {
+    try {
+        const comment = await CommentModel.create({
+                tweet: req.params.tweetId,
+                user:req.user.id,
+                content:req.content,  
+        })
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json("Internal server error while commenting")    
+    }
+}
+
+export const GetTweetById = async(req:any,res:any) => {
+    try {
+        const comment = await TweetModel.find({
+            tweet: req.params.tweetId,
+        }).populate('user', "username")
+        return res.status(200).json("fetch all comments successfully");
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json("Internal server error while fetching comments")  
+    }
+}
+
+export const DeleteTweet = async (req:any,res:any) => {
+    try {
+        const 
+    } catch (e) {
+        
+    }
+} 
