ab502a047ce3d5e9f9e1e7a744197fbd49ad74b4
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-05-11 08:48:47 +0530

    update

--- client/src/components/EditProfile.tsx
+++ client/src/components/EditProfile.tsx
@@ -35,7 +35,9 @@ export const EditModel: React.FC<{
             <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
                 <div className="bg-black text-white p-6 rounded-lg w-full max-w-md">
                     <h2 className="text-xl mb-4 font-semibold">Edit Profile</h2>
-                    
+
+                    <div className="space-y-2">
+                        <label className="">Full Name</label>
 
                         <input
                             value={fullName}
@@ -43,55 +45,71 @@ export const EditModel: React.FC<{
                             placeholder="Full Name"
                             className="mb-2 p-2 w-full border rounded bg-black"
                         />
+                    </div>
+                    <div className="space-y-2">
+                        <label className="">User Name</label>
                         <input
                             value={userName}
                             onChange={(e) => setUserName(e.target.value)}
                             placeholder="Username"
                             className="mb-2 p-2 w-full border rounded bg-black"
                         />
+                    </div>
+                    <div className="space-y-2">
+                        <label className="">Email</label>
                         <input
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             placeholder="Email"
                             className="mb-2 p-2 w-full border rounded bg-black"
                         />
+                    </div>
+                    <div className="space-y-2">
+                        <label className="">Bio</label>
                         <textarea
                             value={bio}
                             onChange={(e) => setBio(e.target.value)}
                             placeholder="Bio"
                             className="mb-2 p-2 w-full border rounded bg-black"
                         />
+                    </div>
+                    <div className="space-y-2">
+                        <label className="">Link</label>
                         <input
                             value={link}
                             onChange={(e) => setLink(e.target.value)}
                             placeholder="Link"
                             className="mb-2 p-2 w-full border rounded bg-black"
                         />
+                    </div>
+                    <div className="space-y-2">
+                        <label className="">Cover Image</label>
                         <input
                             value={Cover_Image}
                             onChange={(e) => setCoverImage(e.target.value)}
                             placeholder="Cover Image URL"
                             className="mb-4 p-2 w-full border rounded bg-black"
                         />
+                    </div>
 
-                        <div className="flex justify-end space-x-2">
-                            <button
-                                onClick={onClose}
-                                className="px-4 py-2  rounded bg-black"
-                            >
-                                Cancel
-                            </button>
-                            <button
-                                type="button"
-                                onClick={onSubmit}
-                                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                            >
+                    <div className="flex justify-end space-x-2">
+                        <button
+                            onClick={onClose}
+                            className="px-4 py-2  rounded bg-black"
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            type="button"
+                            onClick={onSubmit}
+                            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                        >
 
-                                Save
-                            </button>
+                            Save
+                        </button>
 
-                        </div>
-                   
+                    </div>
+
                 </div>
             </div>
         );
